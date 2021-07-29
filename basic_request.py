2d4ad99855f2b1088a1e1b10769eb60eec4bd81b
rohith2597 <54556355+rohith2597@users.noreply.github.com>
2021-07-29 15:52:13 +0200

    Update basic_request.py
    added comments

--- basic_request.py
+++ basic_request.py
@@ -1,8 +1,11 @@
+""" This is a basic Python request code where you can just send strings or messages of json format to the server(API handler)""" 
+
+
 import requests
 
 params = {'param0': 'Rohith', 'param1': 'param1'}
 
-
-response = requests.post('http://127.0.0.1:5000/json', json=params)
+url='http://127.0.0.1:5000/json'
+response = requests.post(url, json=params)
 
 print(response.text)

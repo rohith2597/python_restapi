e6fbbc02066e6d4981c6e08cd7f00bf4cf2ba37d
rohith2597 <54556355+rohith2597@users.noreply.github.com>
2021-08-24 22:40:23 +0200

    update to receive json requests

--- basic_response_Flask.py
+++ basic_response_Flask.py
@@ -22,3 +22,34 @@ def upload_file():
 if __name__ == '__main__':
     # run app in debug mode on port 5000
     app.run(threaded=True)
+
+    
+""" Flask server to handle the incoming JSON format messages """
+from flask import Flask, request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/json', methods=['POST'])
+def upload_file():  
+    
+        print(request.is_json)
+        data = request.get_json()
+        print(data['room'])
+        temp = data['Temperature']
+        print(temp)
+        humi = data['Humidity']
+        print(humi)
+        response=True
+        response_body = {
+            "response":response,    
+        }
+        res = (jsonify(response_body), 200) #Response
+        return res
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(threaded=True)
+
+
+    
+    
